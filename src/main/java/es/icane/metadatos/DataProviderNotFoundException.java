82803530244af6c233092696091332065194c9d1
Sofia <externo.saiz_s@cantabria.es>
2016-03-11 08:36:18 +0100

    Added DataProvider exception and methods deleteCategory, deleteDataSet and deleteDataProvider

---
+++ src/main/java/es/icane/metadatos/DataProviderNotFoundException.java
@@ -0,0 +1,22 @@
+package es.icane.metadatos;
+
+/**
+ * Created by emm13775 on 23/12/2015.
+ */
+public class DataProviderNotFoundException extends Exception {
+
+    public DataProviderNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public DataProviderNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataProviderNotFoundException(String message) {
+        super(message);
+    }
+
+    public DataProviderNotFoundException() {
+    }
+}
